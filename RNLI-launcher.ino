a09b772f1169a55a2c8d9a50ea5a4982fb985a70
Matthew Hamilton <matt.hamilton@ed.ac.uk>
2020-09-22 10:14:24 +0100

    refactor

--- RNLI-launcher.ino
+++ RNLI-launcher.ino
@@ -4,18 +4,12 @@
 #include <Wire.h>
 
 //-------------------------- Configuration --------------------------
-
-
 // edit the config.h tab and enter your Adafruit IO credentials
 // and any additional configuration needed for WiFi, cellular,
 // or ethernet clients.
 #include "config.h"
 
-
-
 //-------------------------- Definitions --------------------------
-
-
 
 // the Adafruit IO username of whomever owns the feed
 #define FEED_OWNER "Rankinjonathan"
@@ -23,15 +17,13 @@
 // set up the `sharedFeed`
 AdafruitIO_Feed *sharedFeed = io.feed("rnli-launcher", FEED_OWNER);
 
-//
 //const int buttonPin = 10;     // the number of the pushbutton pin
 const int ledPin = 13;       // the pin that the LED is attached to
 
-
 // Variables will change:
 
-unsigned long timer;
-unsigned long timerOffset;
+unsigned long currentTime;
+unsigned long startTime;
 
 unsigned long contractLength = 600000;
 unsigned long timeLeft;
@@ -42,7 +34,6 @@ String currentSerialVal = "";   //Put the current serial value in this variable
 
 String tweet = "";
 
-
 boolean stringComplete = false;  // whether the string is complete
 
 int happened = 0;
@@ -50,15 +41,11 @@ int X = 11;
 
 U8X8_SSD1322_NHD_256X64_4W_SW_SPI u8x8(/* clock=*/ 24, /* data=*/ 23, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 5);
 
-
 //-------------------------- Setup --------------------------
 
-
-
-void setup() {
-
+void setup()
+{
   Serial.println("IO test");
-
 
   // initialize the LED as an output:
   pinMode(ledPin, OUTPUT);
@@ -70,7 +57,7 @@ void setup() {
   Serial.begin(115200);
 
   // wait for serial monitor to open
-  while (! Serial);
+  while (!Serial);
 
   // connect to io.adafruit.com
   //  Serial.println("Connecting to Adafruit IO");
@@ -83,7 +70,8 @@ void setup() {
   sharedFeed->onMessage(handleMessage);
 
   // wait for a connection
-  while (io.status() < AIO_CONNECTED) {
+  while (io.status() < AIO_CONNECTED)
+  {
     //    Serial.print(".");
     delay(500);
   }
@@ -94,102 +82,79 @@ void setup() {
   sharedFeed->get();
 
   // setting the start time and print it
-  timer = 0;
+  currentTime = 0;
   Serial.print("Start time = ");
-  Serial.println(timer);
+  Serial.println(currentTime);
 
-  timerOffset = millis();
-
+  startTime = millis();
 
   Serial.print("Finished the setup");
   Serial.println("\n");
-
+  u8x8.begin();
 }
-
-
-
-
 //-------------------------- Loop --------------------------
 
-
-
-
-void loop() {
-
+void loop()
+{
   io.run();
 
+  currentTime = millis();
 
-  timer = millis() - timerOffset;
-
-
-  //-------------------------- If there is a timer runing and an event happened  --------------------------
-
-  if (X > 0 && contractLength > timer && happened == 1) {
+  //-------------------------- If there is a currentTime runing and an event happened  --------------------------
+  
+  if (X > 0 && (currentTime - startTime) < contractLength && happened == 1)
+  {
     Serial.println("Donate £1");
     Serial.print("counter =    ");
     Serial.println(X);
     //-------------------------- Do this section if there is a RNLI launch  --------------------------
+    Serial.print("Time Left in Contract: ");
+    Serial.print((contractLength - (currentTime - startTime))/1000);
+    Serial.println(" seconds");
+    Serial.println(tweet);
 
-  Serial.println(tweet);
+    Serial.print("TEST POINT 1");
+    Serial.println("\n");
 
-  Serial.print("TEST POINT 1");
-  Serial.println("\n");
-  
-  u8x8.begin();
-  u8x8.clear();
-  u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.setCursor(0, 0);
-  u8x8.print(tweet);
-
-  delay(5000);
-
-  //  u8x8.drawString(0, 1, tweet);
-
+    u8x8.clear();
+    u8x8.setFont(u8x8_font_chroma48medium8_r);
+    u8x8.setCursor(0, 0);
+    u8x8.print(tweet);
     //-------------------------- Reset trigger and take 1 off the count  --------------------------
-
     happened = 0;
-    X = X - 1;
-
-
+    X--;
+    delay(5000);
   }
+  else if (X == 0)
+  {
+    Serial.println("All money has been donated");
 
-
-  else if (X == 0) {
-    Serial.println("All money has been donated");;
-
-    timeLeft = contractLength - timer;
+    timeLeft = contractLength - currentTime;
     delay(timeLeft);
     happened = 0;
     X = 0;
-
   }
-
-  else if (timer > contractLength) {
+  else if (currentTime > contractLength)
+  {
     Serial.println("Contract finished - restarting contract");
-    timerOffset = millis();
+    startTime = millis();
 
     happened = 0;
 
-    if (X > 0) {
-
+    if (X > 0)
+    {
       //      Serial.print("counter  c - ");
       X--;
     }
-    else {
+    else
+    {
       X = 0;
     }
   }
-
 }
 
-
-
-void handleMessage(AdafruitIO_Data * data) {
-
+void handleMessage(AdafruitIO_Data * data)
+{
   tweet = data->toString();
-
   happened = 1;
-
-
-
 }
